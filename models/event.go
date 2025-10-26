e2cfae5d8acd9e04fd1b6a3758e3799916bf5f1c
Ronan Conway <ronan.conway@blueoptima.com>
2025-10-26 10:38:47 +0000

    Creating the initial skeleton for creating and saving events to the DB

---
+++ models/event.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"time"
+
+	"github.com/RonanConway/eventsRestAPI/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events = []Event{}
+
+func (event Event) Save() error {
+
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	//Pass in the fields from the event instance into the prepared statement
+	result, err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	event.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
